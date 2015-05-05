ced7a871a6ad988ee835db59d78272f0c6ef4bdb
unknown <micheldc@windowslive.com>
2015-05-05 22:10:03 +0200

    Running version

--- app/src/main/java/ch/keutsa/prototype/root/MainActivity.java
+++ app/src/main/java/ch/keutsa/prototype/root/MainActivity.java
@@ -9,146 +9,129 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.telephony.TelephonyManager;
-import android.util.TypedValue;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import prototype.keutsa.ch.root.R;
 
-/**
- * Sample application demonstrating how to test whether a device is connected,
- * and if so, whether the connection happens to be wifi or mobile (it could be
- * something else).
- *
- * This sample uses the logging framework to display log output in the log
- * fragment (LogFragment).
- */
 public class MainActivity extends FragmentActivity {
 
-    public static final String TAG = "Basic Network Demo";
-    // Whether there is a Wi-Fi connection.
     private static boolean wifiConnected = false;
-    // Whether there is a mobile connection.
     private static boolean mobileConnected = false;
+    private Log log;
 
-    // Reference to the fragment showing events, so we can clear it with a button
-    // as necessary.
-    private LogFragment mLogFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Initialize text fragment that displays intro text.
-        SimpleTextFragment introFragment = (SimpleTextFragment)
-                getSupportFragmentManager().findFragmentById(R.id.intro_fragment);
-        introFragment.setText(R.string.intro_message);
-        introFragment.getTextView().setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16.0f);
-
-        // Initialize the logging framework.
-        initializeLogging();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // When the user clicks TEST, display the connection status.
-            case R.id.test_action:
+        final Button button = (Button) findViewById(R.id.test_action);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 checkNetworkConnection();
-                return true;
-            // Clear the log view fragment.
-            case R.id.clear_action:
-                mLogFragment.getLogView().setText("");
-                return true;
-        }
-        return false;
+            }
+        });
+        final Button button2 = (Button) findViewById(R.id.clear_action);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                log.clear();
+            }
+        });
+        TextView tv = (TextView) findViewById(R.id.outputText);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+
+        log = new Log();
     }
+
     private String networkType() {
         TelephonyManager teleMan = (TelephonyManager)
                 getSystemService(Context.TELEPHONY_SERVICE);
         int networkType = teleMan.getNetworkType();
         switch (networkType) {
-            case TelephonyManager.NETWORK_TYPE_1xRTT: return "1xRTT";
-            case TelephonyManager.NETWORK_TYPE_CDMA: return "CDMA";
-            case TelephonyManager.NETWORK_TYPE_EDGE: return "EDGE";
-            case TelephonyManager.NETWORK_TYPE_EHRPD: return "eHRPD";
-            case TelephonyManager.NETWORK_TYPE_EVDO_0: return "EVDO rev. 0";
-            case TelephonyManager.NETWORK_TYPE_EVDO_A: return "EVDO rev. A";
-            case TelephonyManager.NETWORK_TYPE_EVDO_B: return "EVDO rev. B";
-            case TelephonyManager.NETWORK_TYPE_GPRS: return "GPRS";
-            case TelephonyManager.NETWORK_TYPE_HSDPA: return "HSDPA";
-            case TelephonyManager.NETWORK_TYPE_HSPA: return "HSPA";
-            case TelephonyManager.NETWORK_TYPE_HSPAP: return "HSPA+";
-            case TelephonyManager.NETWORK_TYPE_HSUPA: return "HSUPA";
-            case TelephonyManager.NETWORK_TYPE_IDEN: return "iDen";
-            case TelephonyManager.NETWORK_TYPE_LTE: return "LTE";
-            case TelephonyManager.NETWORK_TYPE_UMTS: return "UMTS";
-            case TelephonyManager.NETWORK_TYPE_UNKNOWN: return "Unknown";
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+                return "1xRTT";
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+                return "CDMA";
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+                return "EDGE";
+            case TelephonyManager.NETWORK_TYPE_EHRPD:
+                return "eHRPD";
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                return "EVDO rev. 0";
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                return "EVDO rev. A";
+            case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                return "EVDO rev. B";
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+                return "GPRS";
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+                return "HSDPA";
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+                return "HSPA";
+            case TelephonyManager.NETWORK_TYPE_HSPAP:
+                return "HSPA+";
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+                return "HSUPA";
+            case TelephonyManager.NETWORK_TYPE_IDEN:
+                return "iDen";
+            case TelephonyManager.NETWORK_TYPE_LTE:
+                return "LTE";
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+                return "UMTS";
+            case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                return "Unknown";
         }
         throw new RuntimeException("New type of network");
     }
-    /**
-     * Check whether the device is connected, and if so, whether the connection
-     * is wifi or mobile (it could be something else).
-     */
+
     private void checkNetworkConnection() {
-        // BEGIN_INCLUDE(connect)
         ConnectivityManager connMgr =
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
         if (activeInfo != null && activeInfo.isConnected()) {
             wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
             mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
-            if(wifiConnected) {
-                Log.i(TAG, getString(R.string.wifi_connection));
+            log.clear();
+            if (wifiConnected) {
+                log.i(getString(R.string.wifi_connection));
                 WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
                 WifiInfo connectionInfo = wifiManager.getConnectionInfo();
                 if (connectionInfo != null) {
-                    Log.i(TAG, "getSSID: " + connectionInfo.getSSID());
-                    Log.i(TAG, "getBSSID: " + connectionInfo.getBSSID());
-                    Log.i(TAG, "getMacAddress: " + connectionInfo.getMacAddress());
-                    //Log.i(TAG, "getFrequency: " + connectionInfo.getFrequency());
-                    Log.i(TAG, "getHiddenSSID: " + connectionInfo.getHiddenSSID());
-                    Log.i(TAG, "getIpAddress: " + connectionInfo.getIpAddress());
-                    Log.i(TAG, "getSupplicantState: " + connectionInfo.getSupplicantState());
-                    Log.i(TAG, "getLinkSpeed: " + connectionInfo.getLinkSpeed());
-                    Log.i(TAG, "getRssi: " + connectionInfo.getRssi());
-                    Log.i(TAG, "getNetworkId: " + connectionInfo.getNetworkId());
+                    log.i("getSSID: " + connectionInfo.getSSID());
+                    log.i("getBSSID: " + connectionInfo.getBSSID());
+                    log.i("getMacAddress: " + connectionInfo.getMacAddress());
+                    //Log.i("getFrequency: " + connectionInfo.getFrequency());
+                    log.i("getHiddenSSID: " + connectionInfo.getHiddenSSID());
+                    log.i("getIpAddress: " + connectionInfo.getIpAddress());
+                    log.i("getSupplicantState: " + connectionInfo.getSupplicantState());
+                    log.i("getLinkSpeed: " + connectionInfo.getLinkSpeed());
+                    log.i("getRssi: " + connectionInfo.getRssi());
+                    log.i("getNetworkId: " + connectionInfo.getNetworkId());
                 }
-            } else if (mobileConnected){
-                Log.i(TAG, getString(R.string.mobile_connection));
-                Log.i(TAG, networkType());
+            } else if (mobileConnected) {
+                log.i(getString(R.string.mobile_connection));
+                log.i(networkType());
             }
         } else {
-            Log.i(TAG, getString(R.string.no_wifi_or_mobile));
+            log.i(getString(R.string.no_wifi_or_mobile));
         }
-        // END_INCLUDE(connect)
     }
 
-    /** Create a chain of targets that will receive log data */
-    public void initializeLogging() {
+    class Log {
+        TextView tv;
 
-        // Using Log, front-end to the logging chain, emulates
-        // android.util.log method signatures.
+        public Log() {
+            tv = (TextView) findViewById(R.id.outputText);
+        }
 
-        // Wraps Android's native log framework
-        LogWrapper logWrapper = new LogWrapper();
-        Log.setLogNode(logWrapper);
+        public void i(String message) {
+            tv.setText(tv.getText() + "\n" + message);
+        }
 
-        // A filter that strips out everything except the message text.
-        MessageOnlyLogFilter msgFilter = new MessageOnlyLogFilter();
-        logWrapper.setNext(msgFilter);
-
-        // On screen logging via a fragment with a TextView.
-        mLogFragment =
-                (LogFragment) getSupportFragmentManager().findFragmentById(R.id.log_fragment);
-        msgFilter.setNext(mLogFragment.getLogView());
+        public void clear() {
+            tv.setText("");
+        }
     }
 }
